d09fe8c76627e8f89d3115077964b25302b41d10
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-01 22:56:37 +0600

    null vs undefined

---
+++ Advance JavaScript/null-vs-undefined.js
@@ -0,0 +1,29 @@
+// undefined 
+let birds;
+console.log(birds);
+
+// undefined
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const addResult = add(13, 82);
+console.log(addResult);
+
+// undefined
+function sub(num1, num2) {
+    console.log(num1, num2);
+}
+const subResult = sub(13);
+console.log(subResult);
+
+// undefined
+const lover = { name: "Tom Hanks", phone: 123456 };
+console.log(lover.gf);
+
+// undefined
+let fun = undefined;
+console.log(fun);
+
+// null
+let make = null;
+console.log(make);
