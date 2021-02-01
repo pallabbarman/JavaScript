94e23017716f2d08f1b8660054d8300d2008dc79
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-01 23:07:43 +0600

    double equal vs triple equal

---
+++ Advance JavaScript/double-vs-triple-equal.js
@@ -0,0 +1,17 @@
+const first = 2;
+const second = "2";
+if (first == second) {                 // double equal only check value
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
+
+const first1 = 2;
+const second1 = "2";
+if (first1 === second1) {               // triple equal check value + type
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
